bcd938756d0ced19a5db3454818ddcfc96e6ab73
ehsanabadi1984 <42084143+ehsanabadi1984@users.noreply.github.com>
2026-09-03 16:44:23 +0330

    Add user account routes

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
+from .views import activity, permissions, profile, settings
+
+
 urlpatterns = [
     path(
         "login/",
@@ -13,5 +16,40 @@ urlpatterns = [
         "logout/",
         auth_views.LogoutView.as_view(),
         name="logout",
+    ),
+    path(
+        "profile/",
+        profile,
+        name="profile",
+    ),
+    path(
+        "password/change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="accounts/password_change.html",
+            success_url="/accounts/password/change/done/",
+        ),
+        name="password_change",
+    ),
+    path(
+        "password/change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="accounts/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
+    path(
+        "permissions/",
+        permissions,
+        name="permissions",
+    ),
+    path(
+        "activity/",
+        activity,
+        name="activity",
+    ),
+    path(
+        "settings/",
+        settings,
+        name="settings",
     ),
 ]
