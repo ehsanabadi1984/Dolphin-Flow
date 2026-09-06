b4e2accabcc1db4526b21e2cf2015c6720dc2eac
ehsanabadi1984 <42084143+ehsanabadi1984@users.noreply.github.com>
2026-09-06 11:40:35 +0330

    Add session timeout behavior tests

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,5 +1,9 @@
+from datetime import timedelta
+
+from django.contrib.sessions.models import Session
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
 from .models import User, UserPreference
 
@@ -43,3 +47,28 @@ class UserPreferenceTests(TestCase):
         UserPreference.objects.create(user=self.user, session_timeout=28800)
         self.client.get(reverse("accounts:settings"))
         self.assertEqual(self.client.session.get_expiry_age(), 28800)
+
+    def test_authenticated_activity_refreshes_session_timeout(self):
+        UserPreference.objects.create(user=self.user, session_timeout=7200)
+        self.client.get(reverse("accounts:settings"))
+
+        session = self.client.session
+        session.set_expiry(30)
+        session.save()
+
+        self.client.get(reverse("accounts:settings"))
+
+        self.assertEqual(self.client.session.get_expiry_age(), 7200)
+
+    def test_expired_session_requires_login(self):
+        UserPreference.objects.create(user=self.user, session_timeout=1800)
+        self.client.get(reverse("accounts:settings"))
+
+        session = Session.objects.get(session_key=self.client.session.session_key)
+        session.expire_date = timezone.now() - timedelta(seconds=1)
+        session.save(update_fields=["expire_date"])
+
+        response = self.client.get(reverse("accounts:settings"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/accounts/login/", response["Location"])
