e055818060b9e1bca5fbac56e4213c162f723053
ehsanabadi1984 <42084143+ehsanabadi1984@users.noreply.github.com>
2026-09-12 10:24:54 +0330

    Add Access workspace list URL

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,73 +17,29 @@ from workflow.process_workspace import (
 )
 from workflow.form_workspace import form_workspace
 from workflow.form_workspace_api import workspace_model_fields
-from workflow.access_workspace import access_security_workspace
+from workflow.access_workspace import (
+    access_security_workspace_list,
+    access_security_workspace,
+)
 
 
 urlpatterns = [
-    path(
-        "",
-        RedirectView.as_view(
-            pattern_name="accounts:login",
-            permanent=False,
-        ),
-        name="root",
-    ),
+    path("", RedirectView.as_view(pattern_name="accounts:login", permanent=False), name="root"),
 
-    path(
-        "admin/workflow/process-workspace/",
-        dolphin_admin_site.admin_view(process_workspace_list),
-        name="process_workspace_list",
-    ),
-    path(
-        "admin/workflow/process-workspace/<int:workflow_id>/",
-        dolphin_admin_site.admin_view(process_workspace),
-        name="process_workspace",
-    ),
+    path("admin/workflow/process-workspace/", dolphin_admin_site.admin_view(process_workspace_list), name="process_workspace_list"),
+    path("admin/workflow/process-workspace/<int:workflow_id>/", dolphin_admin_site.admin_view(process_workspace), name="process_workspace"),
 
-    path(
-        "admin/workflow/form-workspace/<int:workflow_id>/",
-        dolphin_admin_site.admin_view(form_workspace),
-        name="form_workspace",
-    ),
-    path(
-        "admin/workflow/form-workspace/model-fields/",
-        dolphin_admin_site.admin_view(workspace_model_fields),
-        name="workspace_model_fields",
-    ),
+    path("admin/workflow/form-workspace/<int:workflow_id>/", dolphin_admin_site.admin_view(form_workspace), name="form_workspace"),
+    path("admin/workflow/form-workspace/model-fields/", dolphin_admin_site.admin_view(workspace_model_fields), name="workspace_model_fields"),
 
-    path(
-        "admin/workflow/access-security/<int:workflow_id>/",
-        dolphin_admin_site.admin_view(access_security_workspace),
-        name="access_security_workspace",
-    ),
+    path("admin/workflow/access-security/", dolphin_admin_site.admin_view(access_security_workspace_list), name="access_security_workspace_list"),
+    path("admin/workflow/access-security/<int:workflow_id>/", dolphin_admin_site.admin_view(access_security_workspace), name="access_security_workspace"),
 
-    path(
-        "admin/workflow/dynamic/steps/",
-        dolphin_admin_site.admin_view(workflow_dynamic_steps),
-        name="workflow_dynamic_steps",
-    ),
-    path(
-        "admin/workflow/dynamic/transitions/",
-        dolphin_admin_site.admin_view(workflow_dynamic_transitions),
-        name="workflow_dynamic_transitions",
-    ),
-    path(
-        "admin/workflow/dynamic/formfield-model-fields/",
-        dolphin_admin_site.admin_view(formfield_model_fields),
-        name="formfield_model_fields",
-    ),
+    path("admin/workflow/dynamic/steps/", dolphin_admin_site.admin_view(workflow_dynamic_steps), name="workflow_dynamic_steps"),
+    path("admin/workflow/dynamic/transitions/", dolphin_admin_site.admin_view(workflow_dynamic_transitions), name="workflow_dynamic_transitions"),
+    path("admin/workflow/dynamic/formfield-model-fields/", dolphin_admin_site.admin_view(formfield_model_fields), name="formfield_model_fields"),
 
-    path(
-        "admin/",
-        dolphin_admin_site.urls,
-    ),
-    path(
-        "accounts/",
-        include("accounts.urls"),
-    ),
-    path(
-        "operator/",
-        include("operator_panel.urls"),
-    ),
+    path("admin/", dolphin_admin_site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("operator/", include("operator_panel.urls")),
 ]
